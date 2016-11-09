de98c3c2390fbca636fbb9c7a59776e8c9188c74
w3prog <w3prog@gmail.com>
2016-11-09 17:50:54 +0300

    Мелкие исправления

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import os
 from setuptools import setup
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
@@ -13,7 +12,6 @@ setup(
     include_package_data=True,
     license='MIT License',
     description='System for making tourists paths',
-    long_description=README,
     url='none',
     author='w3prog',
     author_email='w3prog@gmail.ru',
